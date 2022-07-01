6862514c5dbc7a0a07557929fbb6b85a075c666a
周涛 <396416959@qq.com>
2022-07-01 17:29:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <router-link to="/" tag="p">Home</router-link>
+      <router-link to="/about" tag="p">About</router-link>
+    </div>
+    <router-view class="router-view" />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  height: 100%;
+}
+#app {
+  height: 100%;
+  display: flex;
+}
+.nav {
+  width: 200px;
+  border-right: 1px solid #efefef;
+  p {
+    font-weight: bold;
+    color: #2c3e50;
+    cursor: pointer;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.router-view {
+  flex: 1;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="home">
+    <el-row :gutter="24">
+      <el-col :xs="24" :sm="12" :md="6" :lg="4" :xl="1">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="4" :xl="1">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="4" :xl="1">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="4" :xl="1">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="4" :xl="1">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="4" :xl="1">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col>
+      <!-- <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+        <div class="grid-content bg-purple-light">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col> -->
+      <!-- <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+        <div class="grid-content bg-purple">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col> -->
+      <!-- <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+        <div class="grid-content bg-purple-light">
+          访问复旦复华认同一条街与列空白是多少个柔荑花然后让它烤鱼炉烤鱼考核人她家人
+        </div>
+      </el-col> -->
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style>
+.el-row {
+  width: 100%;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
